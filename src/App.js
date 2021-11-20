a6cbc5ff8c6c5b8aaeaf9c96922abae6ce3e4f13
Júlio Bento <juliobento237@gmail.com>
2021-11-20 04:06:25 -0300

    create sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 
-import './assets/style/global.scss';
-import styles from './assets/style/Home.module.scss';
+import "./assets/style/global.scss";
+import styles from "./assets/style/Home.module.scss";
+
+import SheetIcon from "./assets/icons/FolhaEfect.svg";
+import HomeIcon from "./assets/icons/HomeIcon.svg";
 
 function App() {
   return (
     <div className={styles.container}>
-      Oi
+      <div className={styles.aside}>
+        <div className={styles.links}>
+          <button className={styles.homeButton}>
+            <img src={HomeIcon} alt="" />
+          </button>
+          <ul>
+            <li className={styles.link}>Destaques</li>
+            <li className={styles.link}>Promoções</li>
+            <li className={styles.link}>Cardápio</li>
+            <li className={styles.link}>Feedbacks</li>
+          </ul>
+        </div>
+        <div className={styles.sheetIcon}>
+          <img src={SheetIcon} alt="" />
+        </div>
+      </div>
+      <div className={styles.sla}></div>
     </div>
   );
 }
